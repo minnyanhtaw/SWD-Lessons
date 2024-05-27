99a1e7d012e4fa26e801fdf92cc8ea506ac2f1c9
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-05-27 15:21:48 +0700

    react day start

---
+++ React/js/main.js
@@ -0,0 +1,9 @@
+const app = document.querySelector("#app");
+
+const heading = document.createElement("h1");
+heading.innerText = "To do App";
+
+const createForm = document.createElement("form");
+
+app.append(createForm);
+app.append(heading);
