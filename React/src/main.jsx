f381d67ce7018fcf7945026389316ea7efc08cb3
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-05-27 16:17:57 +0700

    SWD React Day 67

---
+++ React/src/main.jsx
@@ -0,0 +1,37 @@
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.querySelector("#root");
+
+// const heading = document.createElement("h1");
+// heading.innerText = "To do app";
+
+// rootElement.append(heading);
+
+const tasks = ["Read Js Book", "Eat Less", "Work Out", "Get Up Early"];
+
+const root = createRoot(rootElement);
+root.render(
+  createElement(
+    "div",
+    null,
+    createElement("h1", null, "To Do App"),
+    createElement(
+      "form",
+      null,
+      createElement("input", null, null),
+      createElement("button", null, "add")
+    ),
+    createElement(
+      "ul",
+      null,
+      tasks.map((task, index) =>
+        createElement(
+          "li",
+          { key: index, className: "list mb-5 border border-black" },
+          task
+        )
+      )
+    )
+  )
+);
