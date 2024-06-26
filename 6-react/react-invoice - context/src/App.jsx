1c0146d71f3473939ee9ac548df4d23688bff95f
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-06-26 23:03:07 +0700

    create counter-context

--- 6-react/react-invoice - context/src/App.jsx
+++ 6-react/react-invoice - context/src/App.jsx
@@ -10,38 +10,7 @@ import CheckOutItemList from "./components/CheckOutItemList";
 import Drawer from "./components/Drawer";
 
 const App = () => {
-  const [products, setProduct] = useState([
-    {
-      id: 1,
-      name: "Apple",
-      price: 500,
-      stock: 30,
-    },
-    {
-      id: 2,
-      name: "Orange",
-      price: 600,
-      stock: 23,
-    },
-    {
-      id: 3,
-      name: "Mango",
-      price: 400,
-      stock: 10,
-    },
-    {
-      id: 4,
-      name: "Banana",
-      price: 700,
-      stock: 40,
-    },
-    {
-      id: 5,
-      name: "Lemon",
-      price: 300,
-      stock: 48,
-    },
-  ]);
+
   const [items, setItems] = useState([]);
 
   const [isDrawerOpen, setDrawerOpen] = useState(false);
--- 6-react/react-invoice - context/src/context/ProductProvider.js
+++ 6-react/react-invoice - context/src/context/ProductProvider.js
@@ -3,6 +3,39 @@ import React, { createContext } from "react";
 const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
+  const [products, setProduct] = useState([
+    {
+      id: 1,
+      name: "Apple",
+      price: 500,
+      stock: 30,
+    },
+    {
+      id: 2,
+      name: "Orange",
+      price: 600,
+      stock: 23,
+    },
+    {
+      id: 3,
+      name: "Mango",
+      price: 400,
+      stock: 10,
+    },
+    {
+      id: 4,
+      name: "Banana",
+      price: 700,
+      stock: 40,
+    },
+    {
+      id: 5,
+      name: "Lemon",
+      price: 300,
+      stock: 48,
+    },
+  ]);
+
   return <ProductContext.Provider>{children}</ProductContext.Provider>;
 };
 
