db9872aaddf0d7fb365a9401996fcc96c898f293
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-08-21 21:05:31 +0630

    react day 10 api-homework

--- 6-react/10-api/main.js
+++ 6-react/10-api/main.js
@@ -1,11 +1,26 @@
-import { data } from "autoprefixer";
 import "./style.css";
+const app = document.querySelector("#app");
 const fetchBtn = document.querySelector("#fetchBtn");
+const tasksGroup = document.querySelector("#tasksGroup");
+const tasksTemplate = document.querySelector("#tasksTemplate");
+
+const createTask = (task) => {
+  const taskCard = tasksTemplate.content.cloneNode(true);
+  const taskTitle = taskCard.querySelector(".task-title");
+  const taskDescription = taskCard.querySelector(".task-description");
+  const taskPriority = taskCard.querySelector(".task-priority");
+  const taskDueDate = taskCard.querySelector(".task-due-date");
+  return taskCard;
+};
 
 const handleFetchBtn = () => {
   fetch("http://localhost:5000/tasks")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      data.forEach((task) => {
+        tasksGroup.append(createTask(task));
+      });
+    });
 };
 
 fetchBtn.addEventListener("click", handleFetchBtn);
