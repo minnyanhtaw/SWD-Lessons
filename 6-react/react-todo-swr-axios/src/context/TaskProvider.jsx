ecfbe978533a187cf2a5a4f161d36f98869afb7c
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-09-04 23:38:34 +0630

    use Axios Library

--- 6-react/react-todo-swr-axios/src/context/TaskProvider.jsx
+++ 6-react/react-todo-swr-axios/src/context/TaskProvider.jsx
@@ -1,48 +1,63 @@
+import axios from "axios";
 import React, { createContext, useState } from "react";
 import useSWR, { useSWRConfig } from "swr";
 export const TaskContext = createContext();
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const TaskProvider = ({ children }) => {
+  console.log(import.meta.env.VITE_BASE_URL);
   // const [tasks, setTask] = useState([]);
   const {
     data = [],
     error,
     isLoading,
   } = useSWR("http://localhost:5000/tasks", fetcher);
+
+  const api = axios.create({
+    baseURL: `http://localhost:5000/tasks`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   // console.log(data);
 
   const { mutate } = useSWRConfig();
 
   const addTask = async (newTask) => {
-    const res = await fetch("http://localhost:5000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTask),
-    });
+    await api.post("/", newTask);
+
+    // const res = await fetch("http://localhost:5000/tasks", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newTask),
+    // });
 
     mutate("http://localhost:5000/tasks");
     // setTask([...tasks, newTask]);
   };
   const deleteTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "DELETE",
-    });
+    await api.delete(`/${id}`);
+    // const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    //   method: "DELETE",
+    // });
 
     mutate("http://localhost:5000/tasks");
     // setTask(tasks.filter((task) => task.id !== id));
   };
 
   const doneTask = async (id, currentState) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ isDone: !currentState }),
+    await axios.patch(`/${id}`, {
+      isDone: !currentState,
     });
+    // const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    //   method: "PATCH",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ isDone: !currentState }),
+    // });
 
     mutate("http://localhost:5000/tasks");
     // setTask(
